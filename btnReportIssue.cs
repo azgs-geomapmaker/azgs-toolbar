c9792946c236129bf87aa689fef97ce75dad21b4
Jessica Good Alisdairi <jessica.alisdairi@azgs.az.gov>
2014-06-13 13:31:14 -0700

    Added Report Issue button

---
+++ btnReportIssue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ncgmpToolbar
+{
+    public class btnReportIssue : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public btnReportIssue()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            try {
+                string targetURL = "https://github.com/ncgmp09/azgs-toolbar/issues";
+                System.Diagnostics.Process.Start(targetURL);
+            }
+            catch {
+            }
+        }
+    }
+}
